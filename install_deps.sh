71afa152f100cbbb143ad0747c6bcb5cb6a94ba6
Aditya Kumar Bajpai <77596578+YoCodingMonster@users.noreply.github.com>
2023-10-20 05:11:06 +0530

    Delete install_deps.sh

--- install_deps.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-install etc/modprobe.d/ec_sys.conf /etc/modprobe.d/ec_sys.conf
-install etc/modules-load.d/ec_sys.conf /etc/modules-load.d/ec_sys.conf
-modprobe ec_sys write_support=1
-GRUB_CMDLINE_LINUX_DEFAULT="quiet ec_sys.write_support=1"
-sudo apt-get install python3-tk -y
